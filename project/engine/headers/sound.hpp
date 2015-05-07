7afcac42cb7462cb9be7fcc66b3cf232e6a22b56
Gabriel Hernandez <glhernandez1994@gmail.com>
2015-05-07 13:42:09 -0700

    including more files

---
+++ project/engine/headers/sound.hpp
@@ -0,0 +1,28 @@
+#ifndef SOUND_HPP
+#define SOUND_HPP
+
+#include <string>
+#include <SFML/Audio.hpp>
+#include "GLIncludes.h"
+
+class Sound{
+public:
+    Sound();
+    Sound(std::string _path);
+
+    void load();
+    void playSound();
+    void stopSound();
+
+private:
+    sf::SoundBuffer buffer;
+    sf::Sound audio;
+
+    static const std::string INVALID_EXT;
+    static const std::string INVALID_PATH;
+
+    bool loaded;
+    std::string path;
+};
+
+#endif
