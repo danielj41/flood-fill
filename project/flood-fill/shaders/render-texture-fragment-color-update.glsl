af9c1fcc92cc8bbf08102f77ca998147f8cc5502
Daniel Johnson <danielj41@gmail.com>
2015-05-08 12:56:07 -0700

    no longer hardcodes water simulation color

--- project/flood-fill/shaders/render-texture-fragment-color-update.glsl
+++ project/flood-fill/shaders/render-texture-fragment-color-update.glsl
@@ -8,5 +8,5 @@ void main(){
     vec2 coord = gl_FragCoord.xy / 256.0;
     float r = min(texture2D(uDataTexture, coord).r, 1.0 - 1.0/uSize.y);
     vec2 increasePosition = vec2((uStartPosition.x / uSize.x + 1.0) / 2.0, (uStartPosition.z / uSize.z + 1.0)/2.0);
-    gl_FragColor = vec4(0.0, 0.3, 0.9, clamp(uDTime.y * uDTime.y / dot(coord - increasePosition, coord - increasePosition), uDTime.y/1.5, 1.0));
+    gl_FragColor = vec4(1.0, 1.0, 1.0, clamp(uDTime.y * uDTime.y / dot(coord - increasePosition, coord - increasePosition), uDTime.y/1.5, 1.0));
 }
