720f680ec18d710b9a1889d70e626095939f4bd2
MatheusFaria <matheus.sousa.faria@gmail.com>
2015-05-28 19:39:52 -0700

    Canvas normal map

--- project/engine/source/texture.cpp
+++ project/engine/source/texture.cpp
@@ -17,19 +17,30 @@ void Texture::createTextureFromImage(ImagePtr image, bool mipmap){
     if(image->isPNG()) mode = GL_RGBA;
     else if(image->isBMP()) mode = GL_RGB;
 
-    glTexImage2D(GL_TEXTURE_2D, 0, mode, image->getWidth(),
-        image->getHeight(), 0, mode, GL_UNSIGNED_BYTE,
-        image->getImage());
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     if(mipmap){
+        glTexStorage2D(GL_TEXTURE_2D, 6, GL_RGBA8, image->getWidth(), image->getHeight());
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, image->getWidth(), image->getHeight(),
+                        mode, GL_UNSIGNED_BYTE, image->getImage());
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        glGenerateMipmapEXT(GL_TEXTURE_2D);
     }
     else{
+        glTexImage2D(GL_TEXTURE_2D, 0, mode, image->getWidth(),
+            image->getHeight(), 0, mode, GL_UNSIGNED_BYTE,
+            image->getImage());
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     }
 
     glBindTexture(GL_TEXTURE_2D, 0);
