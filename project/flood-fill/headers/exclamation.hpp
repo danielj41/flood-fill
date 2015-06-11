5263a47401588647164a8a6f1daaa3980118cfec
MatheusFaria <matheus.sousa.faria@gmail.com>
2015-06-10 18:30:56 -0700

    Blinking rotatating exclamation

--- project/flood-fill/headers/exclamation.hpp
+++ project/flood-fill/headers/exclamation.hpp
@@ -1,12 +1,15 @@
 #ifndef EXCLAMATION_HPP
 #define EXCLAMATION_HPP
 
+#include <vector>
+
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_PURE
 #include "glm/glm.hpp"
 
 #include "game_object.hpp"
 #include "object.hpp"
+#include "material.hpp"
 
 #include "pointer_macros.h"
 DEF_PTR(Exclamation, ExclamationPtr);
@@ -26,6 +29,14 @@ private:
     glm::vec3 size;
 
     float timer;
+
+    float rotationAngle, rotationAngleIncrement;
+
+    int materialIndex;
+    double changeMaterialTime, coolDownMaterialTime;
+    bool useBlackMaterial;
+    std::vector<MaterialPtr> availableMaterials;
+    MaterialPtr getNextMaterial();
 };
 
 #endif
--- project/flood-fill/source/exclamation.cpp
+++ project/flood-fill/source/exclamation.cpp
@@ -32,17 +32,51 @@ void Exclamation::setup() {
 
     exclamation2->scale(glm::vec3(.5, 1.5, .5));
     exclamation2->translate(position);
+
+    coolDownMaterialTime = 1;
+    materialIndex = 0;
+    useBlackMaterial = true;
+    availableMaterials.push_back(MaterialManager::getMaterial("Black"));
+    availableMaterials.push_back(MaterialManager::getMaterial("Red"));
+    availableMaterials.push_back(MaterialManager::getMaterial("FlatGreen"));
+    availableMaterials.push_back(MaterialManager::getMaterial("FlatBlue"));
+
+    rotationAngle = 0.0f;
+    rotationAngleIncrement = 0.5f;
 }
 
 void Exclamation::update() {
     timer += TimeManager::getDeltaTime();
     
+    rotationAngle += rotationAngleIncrement;
     exclamation1->loadIdentity();
     exclamation1->scale(glm::vec3(.5));
+    exclamation1->rotate(rotationAngle, glm::vec3(0, 1, 0));  
     exclamation1->translate(glm::vec3(position.x, position.y + cos(timer), position.z));
     
 
     exclamation2->loadIdentity();
     exclamation2->scale(glm::vec3(.5, 1.5, .5));
+    exclamation2->rotate(rotationAngle, glm::vec3(0, 1, 0));  
     exclamation2->translate(glm::vec3(position.x, position.y + cos(timer) + 2.5, position.z));  
+
+    changeMaterialTime += TimeManager::getDeltaTime();
+    if(changeMaterialTime > coolDownMaterialTime){
+        changeMaterialTime = 0;
+        MaterialPtr randomMat = getNextMaterial();
+        exclamation1->setMaterial(randomMat);
+        exclamation2->setMaterial(randomMat);
+    }
+}
+
+MaterialPtr Exclamation::getNextMaterial(){
+    useBlackMaterial = !useBlackMaterial;
+
+    if(useBlackMaterial){
+        return availableMaterials[0];
+    }
+    else{
+        materialIndex = ((materialIndex + 1) % (availableMaterials.size() - 1)) + 1;
+        return availableMaterials[materialIndex];
+    }
 }
